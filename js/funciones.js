af2fc0155fe4920cc0af8d1d93a98a9372768ce0
sittnercamila <camilasittmer09@gmail.com>
2024-04-04 21:26:26 -0300

    botones

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,6 @@
 
 //alert("hola mundo");
 function saludar(nombre){alert("el resultado es:" + 24)}  
+const aux= 72;
+function comparar(numero) {if (numero<aux){alert("el numero es menor")} else {alert("el numero es mayor")};}
+
