378c57e5ff3c923b7253f4f59ad261e7734e943b
sittnercamila <camilasittmer09@gmail.com>
2024-04-11 23:00:37 -0300

    removidos

--- js/funciones.js
+++
@@ -1,6 +0,0 @@
-
-//alert("hola mundo");
-function saludar(nombre){alert("el resultado es:" + 24)}  
-const aux= 72;
-function comparar(numero) {if (numero<aux){alert("el numero es menor")} else {alert("el numero es mayor")};}
-
