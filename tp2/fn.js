62d59cd2d570fa1232c3fc7c1968b6aa90e4d36c
lapeire gabriel <lapeiregabriel@gmail.com>
2024-04-25 21:24:42 -0300

    imagenes

--- tp2/fn.js
+++ tp2/fn.js
@@ -4,22 +4,25 @@ function valor(){
     let idPersonaje1=document.getElementById('idPersonaje1').value
     let idPersonaje2=document.getElementById('idPersonaje2').value
 
-  let img=document.createElement('img');
-  let div=document.createElement('div');
-
+  /*Çlet img=document.createElement('img');
+  img.src = 'idPersonaje1'
+  document.body.appendChild(img)
+  //let div=document.createElement('div');
+*/
 
     fetch(`https://rickandmortyapi.com/api/character/[${idPersonaje1},${idPersonaje2}]`)
             .then (Response=>Response.json())
             .then (data=>{
 
-                  data[0].imag
-                    data[1].image
+                  document.getElementById('data[0].image').innerHTML
+                  data[1].image
                   
             })
+  let img=document.createElement('img');
+    img.src = 'idPersonaje1'
+    document.body.appendChild(img)
+
     }
 
-//<img src="https://e7.pngegg.com/pngimages/118/504/png-clipart-computer-icons-x-mark-wrong-s-cross-sign.png" alt="p1"/>
-
-//<img src="https://thumbs.dreamstime.com/b/el-s%C3%ADmbolo-del-vector-icono-de-la-se%C3%B1al-marca-cotejo-aislada-en-fondo-blanco-comprob%C3%B3-o-muestra-bien-escogida-correcta-144483499.jpg" alt="p2"/>
 
     
