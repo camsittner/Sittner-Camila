5c4574fd3863dcd0a55a33d4472a80411e2a8193
sittnercamila <camilasittmer09@gmail.com>
2024-04-26 18:09:24 -0300

    practica

---
+++ practica/fn.js
@@ -0,0 +1,10 @@
+function boton() {
+    const url="https://rickandmortyapi.com/api/character"
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>
+
+         document.getElementById("primerimput").value
+        
+    )
+}
--- tp2/fn.js
+++ tp2/fn.js
@@ -4,12 +4,6 @@ function valor(){
     let idPersonaje1=document.getElementById('idPersonaje1').value
     let idPersonaje2=document.getElementById('idPersonaje2').value
 
-  /*Çlet img=document.createElement('img');
-  img.src = 'idPersonaje1'
-  document.body.appendChild(img)
-  //let div=document.createElement('div');
-*/
-
     fetch(`https://rickandmortyapi.com/api/character/[${idPersonaje1},${idPersonaje2}]`)
             .then (Response=>Response.json())
             .then (data=>{
@@ -25,14 +19,14 @@ function valor(){
         
 
             
-   /* mainContainer = document.getElementById("Mostrar2");
-    img = document.createElement('img');
-    div = document.createElement("div");
-    if(data[0].gender==data[1].gender) {img.src = "png.bien.png";
-    img.innerHTML = div.innerHTML = "son compatibles";}
-    else {img.src = "png.mal.jpg"; img.innerHTML = div.innerHTML = "no son compatibles";};
-    mainContainer.appendChild(img);
-    mainContainer.appendChild(div);*/
+      /* mainContainer = document.getElementById("Mostrar2");
+        img = document.createElement('img');
+        div = document.createElement("div");
+        if(data[0].gender==data[1].gender) {img.src = "png.bien.png";
+        img.innerHTML = div.innerHTML = "son compatibles";}
+        else {img.src = "png.mal.jpg"; img.innerHTML = div.innerHTML = "no son compatibles";};
+        mainContainer.appendChild(img);
+        mainContainer.appendChild(div);*/
 
     
 
